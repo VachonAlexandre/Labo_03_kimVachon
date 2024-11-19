a00bb445f8a4d60ccb027d29a379ad15c15d67de
VachonAlexandre <201367503@hotmail.com>
2024-11-19 16:47:56 -0500

    Initial Commit

---
+++ Labo_03_kimVachon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Labo_03_kimVachon
+//
+//  Created by Alexandre Vachon (Étudiant) on 2024-11-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Labo_03_kimVachonUITests/Labo_03_kimVachonUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Labo_03_kimVachonUITestsLaunchTests.swift
+//  Labo_03_kimVachonUITests
+//
+//  Created by Alexandre Vachon (Étudiant) on 2024-11-19.
+//
+
+import XCTest
+
+final class Labo_03_kimVachonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
